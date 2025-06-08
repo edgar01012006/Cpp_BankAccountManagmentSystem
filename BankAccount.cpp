5e819b372427df4bd1407f7113474da1aa017cde
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-06-08 22:25:32 +0400

    updated BankAccount.cpp

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -9,14 +9,35 @@ BankAccount::BankAccount() {
     _balance = 0.0;
 }
 
+BankAccount::BankAccount(const BankAccount& src)
+	: _accountHolder { src._accountHolder }, _accountNumber { src._accountNumber }, _balance { src._balance } {
+	//empty
+}
+
+BankAccount::BankAccount(std::string name, int accountNumber, double initialBalance) 
+	: _accountHolder { name }, _accountNumber { accountNumber }, _balance { initialBalance } {
+	//empty
+}
+
 BankAccount::~BankAccount() {
     _accountHolder = "Unknown";
     _accountNumber = 0;
     _balance = 0.0;
 }
 
+BankAccount& BankAccount::operator=(const BankAccount& rhs) {
+	if (this == &rhs) {
+		return *this;
+	} 
+	_accountHolder = rhs._accountHolder;
+	_accountNumber = rhs._accountNumber;
+	_balance = rhs._balance;
+	return *this;
+}
+
 void BankAccount::deposit(double amount) {
     if (amount < 0) {
+		std::cout << "Can't deposit negative amount\n";
         return;
     }
     _balance += amount;
@@ -28,7 +49,7 @@ bool BankAccount::withdraw(double amount) {
         return false;
     }
     if (amount > _balance) {
-        std::cout << "Can't withdraw amount bigger than balance\n";
+        std::cout << "Can't withdraw bigger ammount than balance\n";
         return false;
     }
     else {
@@ -40,7 +61,4 @@ bool BankAccount::withdraw(double amount) {
 
 void BankAccount::display() const {
     std::cout << "AccountHolder's name is " << _accountHolder << "\t AccountNumber is " << _accountNumber << "\t Account balance = " << _balance;
-}
-
-int main() {
 }
